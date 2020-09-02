35515bd12d9c5bcee65c27e8b171795624ab2ab0
Miguel Rodriguez <miarodriguezcu@unal.edu.co>
2020-09-02 08:41:03 -0500

    underflow y overlfow bien hecho

--- 02-09-2020-errores/errores-numericos.cpp
+++ 02-09-2020-errores/errores-numericos.cpp
@@ -1,28 +1,25 @@
-#include <iostream>
-#include <cstdlib>
+#include <iostream>             
 
-int main (int argc, char **argv)
+//Este programa muesra el underlfow y overflow es decir muestra el numero
+//para el cual la maquina ya no puede mostrar más numeros porque son o
+//muy pequeños (10⁻⁴⁵) o muy grandes (10³⁸)
 
+int main(int argc, char **argv)
 {
-  // argc : contar el numero de argumentos en la linea de comandos
-  //argv : Guardar los valores de esoso argumentos
-  std::cout << argv[0] <<"\n";
-  std::cout << argv[1] <<"\n";
+  float under=1.0, over=1.0;          //Declara variables
+  int N=150;                          //Declara Variable
 
-  //exploracion de under cover
-  double under = 1.0, over = 1.0;
-  int N = std::atoi(argv[1]);
-  std::cout << N << "\n";
-  //float under=1.0, over=1.0;
-  //int N=150;
-  for(int ii=0; ii<N; ii++)
+  for(int ii=0; ii<N; ii++)           //Dentro del for introduce ii
     {
-      under/=2.0;
-      over*=2.0;
-      //std::cout<<ii
-      //         <<"\t"<<under
-      //         <<"\t"<<over
-      //	       <<"\n";
+      under /=2.0;                    //Divide under por 2 recursivamente
+      over *=2.0;                     //Multiplica over por 2 recursivamente
+
+      std::cout << ii
+		<< "\t" << under      //Imprime las variables señaladas
+		<< "\t" << over       //"\t" añade un tab 
+		<< "\n";              //"\n" añade una linea
     }
+  
   return 0;
 }
+
